f1687036b50e93edc328476813038efa0a0df408
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2023-02-26 17:33:10 -0300

    Video 3.2

--- src/main/java/com/alura/mvc/mudi/WebSecurityConfig.java
+++ src/main/java/com/alura/mvc/mudi/WebSecurityConfig.java
@@ -24,6 +24,7 @@ public class WebSecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 			.authorizeHttpRequests((requests) -> requests
+				.requestMatchers("/home/**").permitAll()
 				.anyRequest().authenticated()
 			)
 			.formLogin(form -> form
@@ -33,6 +34,7 @@ public class WebSecurityConfig {
 			)
 			.logout(logout -> logout                                                
 	            .logoutUrl("/logout")
+	            .logoutSuccessUrl("/home")
             );
 
 		return http.build();
--- src/main/java/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/com/alura/mvc/mudi/controller/HomeController.java
@@ -25,18 +25,8 @@ public class HomeController {
 	@GetMapping
 	public String home(Model model, Principal principal) {
 		
-		List<Pedido> pedidos = pedidoRepository.findAll();
+		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.ENTREGADO);
 		model.addAttribute("pedidos",pedidos);
-		return "home";
-	}
-	
-
-	@GetMapping("/{status}")
-	public String porStatus(@PathVariable("status") String status,Model model) {
-		
-		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.valueOf(status.toUpperCase()));
-		model.addAttribute("pedidos",pedidos);
-		model.addAttribute("status", status);
 		return "home";
 	}
 	
